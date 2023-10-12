c1254f78d76928607b3bd578dc9ce7a0e3297d17
Vitali Kalaskou <kolosokvit@gmail.com>
2023-10-12 10:45:05 +0300

    Complete Queue tasks

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,23 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> resultDeque = new ArrayDeque<>();
+        resultDeque.add(firstQueue.remove());
+        resultDeque.add(firstQueue.remove());
+        resultDeque.add(secondQueue.remove());
+        resultDeque.add(secondQueue.remove());
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.add(resultDeque.removeLast());
+            resultDeque.add(firstQueue.remove());
+            resultDeque.add(firstQueue.remove());
+            secondQueue.add(resultDeque.removeLast());
+            resultDeque.add(secondQueue.remove());
+            resultDeque.add(secondQueue.remove());
+        }
+        return resultDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,33 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishOrder = new ArrayList<>();
+        Queue<Integer> dishesOnTheTable = new ArrayDeque<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishesOnTheTable.add(i);
+        }
+        Queue<Integer> temporaryDeque = new ArrayDeque<>();
+        int stepCounter = 0;
+        while (dishOrder.size() != numberOfDishes) {
+            stepCounter++;
+            if (stepCounter % everyDishNumberToEat == 0) {
+                dishOrder.add(dishesOnTheTable.remove());
+            } else {
+               temporaryDeque.add(dishesOnTheTable.remove());
+            }
+            if (dishesOnTheTable.isEmpty()) {
+                dishesOnTheTable.addAll(temporaryDeque);
+                temporaryDeque.clear();
+            }
+        }
+        return dishOrder;
+    }
+
+    public static void main(String[] args) {
+        DishOrderDeterminer dishOrderDeterminer = new DishOrderDeterminer();
+        dishOrderDeterminer.determineDishOrder(10, 3);
     }
 }
